78c27d53260dabca3abb1937e764f691c7a5d47a
PSC2313 <pedrosantoscintra07@gmail.com>
2024-05-14 17:21:14 -0300

    update

--- FRONT/App.js
+++ FRONT/App.js
@@ -9,6 +9,7 @@ import Cadastro from "./NavegacaoDasTelas/CADASTRO/Cadastro.js";
 import Agendamento from "./NavegacaoDasTelas/Agendamento.js";
 import CadastroCarroScreen from "./NavegacaoDasTelas/CADASTRO/CadastroCarro.js";
 import TelaDeNotificacao from "./NavegacaoDasTelas/TelaDeNotificacao.js";
+import Abertura from "./NavegacaoDasTelas/Abertura.js";
 
 
 const Stack = createStackNavigator();
@@ -16,12 +17,17 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Navigator initialRouteName="Abertura">
         <Stack.Screen
          name="LoginScreen"
          component={LoginScreen}
          options={{ headerShown: false }}
        /> 
+       <Stack.Screen
+       name="Abertura"
+       component={Abertura}
+       options={{headerShown: false}}
+       />
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
