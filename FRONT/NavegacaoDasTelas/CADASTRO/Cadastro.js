b949a2c4d990e530a477d1b076f46b785c7246a4
Victor Petrucelli <45275534+Petrucellivictor@users.noreply.github.com>
2024-05-24 14:07:18 -0300

    Update Cadastro.js
    Personalização da tela e ajustes para IOS

--- FRONT/NavegacaoDasTelas/CADASTRO/Cadastro.js
+++ FRONT/NavegacaoDasTelas/CADASTRO/Cadastro.js
@@ -1,42 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput,Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Button, Platform,  Modal, selectedValue, modalVisible, handlePickerChange,confirmarEscolha } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons';
 
-
-const Cadastro = ({navigation}) => {
-  //Setando os metodos das informações do usuário
+const Cadastro = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [nome, setNome] = useState('');
-  const [cpf, setCpf] = useState(''); 
+  const [cpf, setCpf] = useState('');
   const [dia, setDia] = useState('');
   const [mes, setMes] = useState('');
   const [ano, setAno] = useState('');
   const [sexo, setSexo] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
   const [erro, setErro] = useState('');
 
-  const botaologin=()=>{
+  const handlePickerChange = (itemValue) => {
+    setSexo(itemValue);
+  };
+
+    const confirmarEscolha = () => {
+      setSelectedValue(sexo);
+      setModalVisible(false);
+      navigation.navigate('Cadastro');
+    };
+
+  const botaologin = () => {
     navigation.navigate('LoginScreen');
   }
 
-  //Formatando a data de nacimento para o banco aceitar de forma normal
   const formatarDataDeNascimento = (dia, mes, ano) => {
     return `${ano}-${mes}-${dia}`;
   };
 
+
   const validarData = (dia, mes, ano) => {
-    //Valida a data se a pessoa pasar o ano, mês e dia assim = dd/mm/aaaa
     const dataRegex = /^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/](19\d{2}|20[01]\d|202[0-4])$/;
-  
+
     if (dataRegex.test(`${dia}/${mes}/${ano}`)) {
       return 'Data de nascimento inválida.';
     } else {
       return '';
     }
   };
+    
   
-  
-  //Cria o usuario no banco passando pelo Back End
   const Criar = () => {
     setErro('');
 
@@ -45,16 +54,14 @@ const Cadastro = ({navigation}) => {
       return;
     }
 
-    //Regex sendo chamada
     validarData();
 
     if (erro) {
       return;
     }
 
-    const formattedDate = formatarDataDeNascimento(dia,mes,ano);
+    const formattedDate = formatarDataDeNascimento(dia, mes, ano);
 
-    //setando os dados
     const dados = {
       "nome": nome,
       "email": email,
@@ -64,13 +71,8 @@ const Cadastro = ({navigation}) => {
       "sexo": sexo
     };
 
-    //console.log(dados);
-
-    
-
-    fetch('http://10.110.12.3:8080/api/usuarios', { //metodo para chamar a API usando o feth
-      method: 'POST', //Usamos o POST para postar no banco as informações
-
+    fetch('http://10.110.12.11:8080/api/usuarios', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -89,22 +91,31 @@ const Cadastro = ({navigation}) => {
     .catch(error => {
       console.error('Ocorreu um erro ao tentar criar usuário:', error);
     });
-    
   };
+ 
 
   return (
-    //Tela para Colocar as informações do usuário
     <View style={styles.container}>
-       <Text style={styles.label}>Digite o EMAIL:</Text>
+      <View style={styles.TituloCadastro}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={24} color="white" marginTop={100} marginLeft={20} />
+        </TouchableOpacity>
+        <Text style={styles.header}>Cadastro</Text>
+        <Image source={{ uri: 'https://github.com/SSancaSH-Projetos/MyAuto/blob/main/MY%20AUT.png?raw=true' }} style={styles.logo} />
+      </View>
+      <View style={styles.containerText}>
+        <Text style={styles.label}>Digite seu E-MAIL:</Text>
+      </View>
       <TextInput
         style={styles.input}
-        placeholder="EMAIL"
+        placeholder="Email"
         keyboardType="email-address"
         value={email}
         onChangeText={setEmail}
       />
-
-      <Text style={styles.label}>Digite o NOME:</Text>
+      <View style={styles.containerText}>
+        <Text style={styles.label}>NOME:</Text>
+      </View>
       <TextInput
         style={styles.input}
         placeholder="NOME"
@@ -112,22 +123,23 @@ const Cadastro = ({navigation}) => {
         value={nome}
         onChangeText={setNome}
       />
-
-       <Text style={styles.label}>Digite o CPF:</Text>
-      <TextInput
-        style={styles.input}
+      
+      <View style={styles.containerText}>
+        <Text style={styles.label}>CPF:</Text>
+      </View>
+      <TextInput style={styles.input}
         placeholder="CPF"
         keyboardType="numeric"
         maxLength={11}
         value={cpf}
         onChangeText={setCpf} 
       />
-      {/*Botão para setar a data*/}
-      <Text style={styles.label}>DATA DE NASCIMENTO:</Text>
+      <View style={styles.containerText}>
+        <Text style={styles.label}>DATA DE NASCIMENTO:</Text>
+      </View>
       <View style={styles.dataNascimentoInput}>
-        {/*Dia*/}
         <TextInput
-          style={[styles.input, { width: '30%' }]}
+          style={[styles.input, { width: '25%' }]}
           placeholder="DD"
           keyboardType="numeric"
           maxLength={2}
@@ -135,7 +147,6 @@ const Cadastro = ({navigation}) => {
           onChangeText={setDia}
         />
         <Text style={{ paddingHorizontal: 5 }}>/</Text>
-        {/*Mês*/}
         <TextInput
           style={[styles.input, { width: '30%' }]}
           placeholder="MM"
@@ -145,7 +156,6 @@ const Cadastro = ({navigation}) => {
           onChangeText={setMes}
         />
         <Text style={{ paddingHorizontal: 5 }}>/</Text>
-        {/*Ano*/}
         <TextInput
           style={[styles.input, { width: '40%' }]}
           placeholder="AAAA"
@@ -155,37 +165,61 @@ const Cadastro = ({navigation}) => {
           onChangeText={setAno}
         />
       </View>
+      
+      <View style={styles.container}>
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.input2}>
+        <Text style={styles.pickerText}>{selectedValue || 'Selecionar'}</Text>
+      </TouchableOpacity>
 
-        <Text style={styles.label}>QUAL É SEXO:</Text>
-        {/*Lista para escolher o sexo da pessoa*/}
-        <View style={styles.input}>
-        <Picker
-        selectedValue={sexo}
-        onValueChange={(itemValue) => setSexo(itemValue)}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
       >
-        <Picker.Item label='Celecionar' value={(false)}/>
-        <Picker.Item label="Homem" value="Homem"/>
-        <Picker.Item label="Mulher" value="Mulher" />
-        <Picker.Item label="Prefiro Não Opinar" value="Prefiro Não Opinar"/>
-      </Picker>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Picker
+              selectedValue={sexo}
+              onValueChange={handlePickerChange}
+              style={styles.picker}
+              mode="dropdown" // Definindo o modo como dropdown para iOS
+            >
+              <Picker.Item label="Selecionar" value="" />
+              <Picker.Item label="Masculino" value="Homem" />
+              <Picker.Item label="Feminino" value="Mulher" />
+              <Picker.Item label="Prefiro Não Opinar" value="Prefiro Não Opinar" />
+            </Picker>
+
+            <Button title="Confirmar Escolha" onPress={confirmarEscolha} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+      
+
+      <View style={styles.containerText}>
+        <Text style={styles.label}>Defina uma senha:</Text>
       </View>
-
-
-      <Text style={styles.label}>Digite a SENHA:</Text>
       <TextInput
-      style={styles.input}
-      placeholder='SENHA'
-      value={senha}
-      onChangeText={setSenha}
-      secureTextEntry={true}
+        style={styles.input}
+        placeholder='SENHA'
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
       />
-      <Button title="Criar Conta" onPress={Criar} />
+      <View style={styles.ButtonStyle}>
+        <TouchableOpacity onPress={Criar}>
+          <Text style={styles.buttonText}>Criar Conta</Text>
+        </TouchableOpacity>
+      </View>
 
       {erro !== '' && <Text style={styles.error}>{erro}</Text>}
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
@@ -194,9 +228,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+  header:{
+    color: 'white',
+    fontSize: 30,
+    marginLeft: '25%',
+    marginTop: '25%',   
+  },
+  TituloCadastro:{
+    backgroundColor:'#0B0020',
+    width:'115%',
+    height: 180,
+    padding: 10,
+    marginBottom: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    top: 5,
+    marginBottom: 10
+  },
   label: {
     fontSize: 16,
-    marginBottom: 5,
+    color: 'white',
+    padding: 10,
   },
   input: {
     height: 40,
@@ -205,6 +257,18 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    borderRadius:30,
+    backgroundColor: '#FAFBA7'
+  },
+  input2: {
+    height: 40,
+    width: 370,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius:30,
+    backgroundColor: '#FAFBA7'
   },
   resultContainer: {
     marginTop: 20,
@@ -212,16 +276,70 @@ const styles = StyleSheet.create({
   dataNascimentoInput: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
-  },
-  resultLabel: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 8,
+    padding: 8,
   },
   error: {
     color: 'red',
     marginTop: 10,
+  },
+  containerText:{
+    width: '100%',
+    backgroundColor:'#0B0020',
+    borderRadius: 30,
+    padding: '1%',
+    margin: 5
+  },
+  ButtonStyle:{
+    width: '90%',
+    backgroundColor: 'green',
+    borderRadius: 30,
+    padding: 12,
+    marginTop: 10,
+    alignItems: 'center',
+    marginBottom: '30%'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  logo: {
+    width: 100,
+    height: 90,
+    resizeMode: 'contain',
+    marginLeft: 50,
+    marginTop: 90
+  },
+  pickerText: {
+    fontSize: 20,
+    alignItems: 'center',
+    marginLeft: '40%',
+    marginTop: 8,
+    color: 'black'
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(1, 0, 0, 0.5)',
+    
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 30,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picker: {
+    width: '100%',
+    ...Platform.select({
+      ios: {
+        color: 'black',
+      },
+    }),
   },
 });
 
